7ce4bae87c01b412a81c95c84cd5b1f7e46d44f1
nsouzarj <nsouzarj@outlook.com>
2022-01-31 10:00:37 -0300

    Elaboraçãp de mais métodos

--- src/main/java/br/eti/nsouza/entidades/OutraParteJSON.java
+++ src/main/java/br/eti/nsouza/entidades/OutraParteJSON.java
@@ -11,19 +11,15 @@ public class OutraParteJSON {
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	@Override
 	public String toString() {
 		return "OutraParteJSON [id=" + id + ", name=" + name + "]";
