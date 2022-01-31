27a39ef46464a014ed32beaddc8917f198edc5bc
nsouzarj <nsouzarj@outlook.com>
2022-01-30 23:19:51 -0300

    Elaboraçãp de mais métodos

--- src/main/java/br/eti/nsouza/entidades/ClienteJSON.java
+++ src/main/java/br/eti/nsouza/entidades/ClienteJSON.java
@@ -3,27 +3,21 @@ package br.eti.nsouza.entidades;
 public class ClienteJSON {
 	private String id;
 	private String name;
-
 	public ClienteJSON() {
 		// TODO Auto-generated constructor stub
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	@Override
 	public String toString() {
 		return "ClienteJSON [id=" + id + ", name=" + name + "]";
